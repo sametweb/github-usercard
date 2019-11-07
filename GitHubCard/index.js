2a0560e8f8fa914d385975cb6c9f0744a01429a7
sametweb <sametmutevellioglu@gmail.com>
2019-11-07 00:02:01 -0800

    final

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,38 @@
            https://api.github.com/users/<your name>
 */
 
+const myUsername = "sametweb";
+
+const printGitHubCards = username => {
+  axios
+    .get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      console.log(response);
+      let cards = $(".cards");
+      cards.append(githubCard(response.data));
+      return response.data.followers_url;
+    })
+    .then(response => {
+      $(".cards").append(
+        $(
+          `<h1 style="font-size: 2rem; margin-bottom: 20px;">${username}'s Followers</h1>`
+        )
+      );
+
+      axios
+        .get(response)
+        .then(res =>
+          res.data.forEach(item => {
+            let cards = $(".cards");
+            cards.append(githubCard(item));
+          })
+        )
+        .catch(error => console.log("Followers error:", error));
+    })
+
+    .catch(error => console.log("My Profile Error", error));
+};
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +56,13 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -46,6 +84,57 @@ const followersArray = [];
 
 */
 
+const githubCard = object => {
+  let card = $('<div class="card"></div>');
+  let img = $("<img />");
+  let cardInfo = $('<div class="card-info"></div>');
+  let name = $('<h3 clas="name"></h3>');
+  let username = $('<p class="username"></p>');
+  let location = $("<p></p>");
+  let profile = $("<p></p>");
+  let profileURL = $("<a></a>");
+  let followers = $("<p></p>");
+  let following = $("<p></p>");
+  let bio = $("<p></p>");
+
+  img.attr("src", object.avatar_url);
+  !object.name ? img.css({ width: "50px", height: "50px" }) : null;
+  object.name ? name.text(object.name) : null;
+  username.text(object.login);
+  object.location ? location.text(`Location: ${object.location}`) : null;
+  profile.text("Profile: ");
+  profileURL.attr("href", object.html_url);
+  profileURL.text(object.html_url);
+  object.followers ? followers.text(`Folowers: ${object.followers}`) : null;
+  object.following ? following.text(`Following: ${object.following}`) : null;
+  object.bio ? bio.text(object.bio) : null;
+
+  // cardInfo.append([
+  //   name,
+  //   username,
+  //   location,
+  //   profile,
+  //   followers,
+  //   following,
+  //   bio
+  // ]);
+  // profile.append(profileURL);
+  card.append([
+    img,
+    cardInfo.append([
+      name,
+      username,
+      location,
+      profile.append(profileURL),
+      followers,
+      following,
+      bio
+    ])
+  ]);
+
+  return card;
+};
+
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
@@ -53,3 +142,4 @@ const followersArray = [];
   luishrd
   bigknell
 */
+printGitHubCards(myUsername);
